28551105977ab13225c199f4b605a193f951f280
Francisco <fbernaly@gmail.com>
2020-04-03 13:38:11 -0700

    AppCenter for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,20 @@
 import UIKit
 import Flutter
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+import AppCenterDistribute
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+  
+  override func application(_ application: UIApplication,
+                            didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    MSAppCenter.start("f9fbee81-875e-4493-8f1f-ccc47fed1323", withServices:[
+      MSDistribute.self,
+      MSAnalytics.self,
+      MSCrashes.self
+    ])
     
     if let controller = self.window.rootViewController as? FlutterViewController {
       let channel = FlutterMethodChannel(name: "com.openworkbook.orientation", binaryMessenger: controller.binaryMessenger)
